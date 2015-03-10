c95ac6f50f728ffa007185f986e91c581461f875
Rogerio Pontes <idesignerweb@gmail.com>
2015-03-10 10:52:09 -0300

    Delete pasta estagiários

--- estagiarios/404.php
+++
@@ -1,8 +0,0 @@
-<html><head>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <title>Minha Concessionária | Página não encontrada | Page Not Found</title>
-  </head>
-	<body>
-              404  
-	</body>
-</html>
